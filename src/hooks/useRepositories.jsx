426719060dc6ff06fd25c1aa74e3f0be9e32e98f
Elvann <elvann.abendroth@gmail.com>
2024-01-22 17:23:42 +0200

    ✅10.12

--- src/hooks/useRepositories.jsx
+++ src/hooks/useRepositories.jsx
@@ -4,17 +4,17 @@ import { useQuery } from '@apollo/client'
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState()
-  //const [loading, setLoading] = useState(false)
 
+  // eslint-disable-next-line no-unused-vars
   const { data, error, loading } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' })
 
   const fetchRepositories = async () => {
-    setRepositories(data.repositories)
+    setRepositories(data?.repositories)
   }
 
   useEffect(() => {
     fetchRepositories()
-  }, [])
+  }, [data])
 
   return { repositories, loading, refetch: fetchRepositories }
 }
