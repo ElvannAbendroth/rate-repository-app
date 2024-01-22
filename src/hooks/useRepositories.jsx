70b9ab5e7284aa3862fa131eee7c4a351cbb0418
Elvann <elvann.abendroth@gmail.com>
2024-01-22 16:02:28 +0200

    ✅10.11

--- src/hooks/useRepositories.jsx
+++ src/hooks/useRepositories.jsx
@@ -4,18 +4,12 @@ import { useQuery } from '@apollo/client'
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState()
-  const [loading, setLoading] = useState(false)
+  //const [loading, setLoading] = useState(false)
 
-  //const { data, error, loading } = useQuery(GET_REPOSITORIES);
+  const { data, error, loading } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' })
 
   const fetchRepositories = async () => {
-    setLoading(true)
-
-    const response = await fetch(`http://192.168.101.131:5000/api/repositories`)
-    const json = await response.json()
-
-    setLoading(false)
-    setRepositories(json)
+    setRepositories(data.repositories)
   }
 
   useEffect(() => {
